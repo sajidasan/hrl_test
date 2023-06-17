938303ecd33681cb861506a64506fcd08a65b669
sajidasan <100092665+sajidasan@users.noreply.github.com>
2023-06-17 11:56:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  puts("Hello, Saji! It's Artume testing stuff");
+  puts("Hello, Saji! It's Artume testing stuff!!");
   return EXIT_SUCCESS;
 }
